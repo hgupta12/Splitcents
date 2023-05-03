7db72a43a4d94c2acad88a5b85b61f6e40cfd6bc
farhan <syedfarhan0210@gmail.com>
2023-05-03 12:37:21 +0530

    googleauth

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,24 @@ import Login from './components/auth/Login'
 import Home from './components/Home';
 import {useContext} from 'react';
 import { AuthContext } from './components/auth/AuthContext';
-//import { onAuthStateChanged } from 'firebase/auth';
-//import { auth } from './firebase';
-
 function App() {
   const {currentUser} = useContext(AuthContext);
   const RequireAuth = ({ children }) => {
     return currentUser ? children : <Navigate to="/login" />;
   };
-
- 
- return (
+return (
     <>
-  
-      <div className="text-3xl font-bold underline">Splitcents</div>
+    <div className="text-3xl font-bold underline">Splitcents</div>
       <Routes>
       <Route path='/' element={
                 <RequireAuth>
                   <Home />
                 </RequireAuth>}/>
        <Route path="/login" element={<Login/>}/>
-       
-              
-            
-       
-      
-</Routes>
+       </Routes>
 
  </>
   
   )
 }
-
 export default App;
